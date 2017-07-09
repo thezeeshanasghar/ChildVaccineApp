8fec1830f131354e8891af9527e177d0d6fd0062
thezeeshanasghar <thezeeshanasghar@gmail.com>
2017-07-09 16:50:11 +0500

    regenrate EF model

--- VaccineDose/VDModel.Context.cs
+++ VaccineDose/VDModel.Context.cs
@@ -30,8 +30,8 @@ namespace VaccineDose
         public virtual DbSet<Doctor> Doctors { get; set; }
         public virtual DbSet<Dose> Doses { get; set; }
         public virtual DbSet<DoseRule> DoseRules { get; set; }
-        public virtual DbSet<Vaccine> Vaccines { get; set; }
         public virtual DbSet<Schedule> Schedules { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Vaccine> Vaccines { get; set; }
     }
 }
