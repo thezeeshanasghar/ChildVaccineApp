751edb504d84a3680198c6e16f7e9704f0b6f83e
Muneeb Ur Rehman <munni7474@gmail.com>
2017-08-07 08:48:37 -0700

    show child list to parent and hide injection from schedule

--- VaccineDose/js/common.js
+++ VaccineDose/js/common.js
@@ -88,7 +88,7 @@ function SetMainNav() {
         markup += '<div class="btn-group btn-group-justified">';
         markup += '     <a href="#" class="btn btn-primary btn-lg">Find Child Specialist</a>';
         markup += '     <a href="#" class="btn btn-primary btn-lg">Child safety</a>';
-        markup += '     <a href="#" class="btn btn-primary btn-lg">My Children</a>';
+        markup += '     <a href="child.html" class="btn btn-primary btn-lg">My Children</a>';
         markup += '     <a href="#" class="btn btn-primary btn-lg" onclick="return logout()" id="menu-logout">Logout</a>';
         markup += '</div>';
     } else {
