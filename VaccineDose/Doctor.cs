2d7a274424f99a203ffd0b07cee6647e627fc808
Muneeb Ur Rehman <munni7474@gmail.com>
2017-08-15 22:32:21 +0500

    Add validUpto column in doctor and show calender in approved doctor action

--- VaccineDose/Doctor.cs
+++ VaccineDose/Doctor.cs
@@ -51,6 +51,8 @@ public partial class Doctor
 
     public string PhoneNo { get; set; }
 
+    public Nullable<System.DateTime> ValidUpto { get; set; }
+
 
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
