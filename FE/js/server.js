f56b796390336925058c4d0805d344038d1e8e1b
Muneeb Ur Rehman <munni7474@gmail.com>
2017-08-27 10:59:05 +0500

    save due to encoding issue

--- FE/js/server.js
+++ FE/js/server.js
@@ -1,7 +1,7 @@
-var SERVER_IP = 'http://www.vac.afz-sol.com';
-var SERVER_PORT = '80';
-//var SERVER_IP = 'http://localhost';
-//var SERVER_PORT = '4309';
+//var SERVER_IP = 'http://www.vac.afz-sol.com';
+//var SERVER_PORT = '80';
+var SERVER_IP = 'http://localhost';
+var SERVER_PORT = '4309';
 var SERVER_BASE_PATH = 'api';
 
 var SERVER = SERVER_IP + ':' + SERVER_PORT + '/' + SERVER_BASE_PATH + '/';
