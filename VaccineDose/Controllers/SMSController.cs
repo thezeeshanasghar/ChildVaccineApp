a0af551938cfbc9fc0fc3cc3139b0eeab25f21c1
Muneeb Ur Rehman <munni7474@gmail.com>
2017-09-18 00:01:08 +0500

    api to combile all sms of pending status

--- VaccineDose/Controllers/SMSController.cs
+++ VaccineDose/Controllers/SMSController.cs
@@ -36,5 +36,37 @@ namespace VaccineDose.Controllers
             }
 
         }
+
+        [HttpGet]
+        [Route("api/sms/sms-string")]
+        public Response<string> GetSMS()
+        {
+            try
+            {
+                using (VDConnectionString entities = new VDConnectionString())
+                {
+                    var dbSMS = entities.SMS.Where(x => x.Status == "PENDING").ToList();
+                    if (dbSMS.Count > 0)
+                    {
+                        var SMS = "";
+                        foreach (var sms in dbSMS)
+                        {
+                            SMS += sms.MobileNumber + ":" + sms.Message + "_";
+                        }
+                        return new Response<string>(true, null, SMS);
+                    }
+                    else
+                    {
+                        return new Response<string>(false, null, null);
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                return new Response<string>(false, GetMessageFromExceptionObject(e), null);
+            }
+
+        }
     }
 }
