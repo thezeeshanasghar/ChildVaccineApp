e1f74d478af98830143d6821408f18ef8d54843f
thezeeshanasghar <thezeeshanasghar@gmail.com>
2017-11-21 13:12:59 +0500

    remove library extra files and set initial contry

--- FE/login.js
+++ FE/login.js
@@ -1,66 +1,70 @@
 ﻿$(document).ready(function () {
     HideAlert();
     localStorage.clear();
-    $("#MobileNumber").intlTelInput();
+    $("#MobileNumber").intlTelInput(
+        {
+            initialCountry: "PK"
+        }
+    );
 });
 
 function Login() {
-        $('#form1').validator('validate');
-        var validator = $('#form1').data("bs.validator");
-        if (validator.hasErrors())
-            return false;
+    $('#form1').validator('validate');
+    var validator = $('#form1').data("bs.validator");
+    if (validator.hasErrors())
+        return false;
 
-        $("#btnSignIn").button('loading');
-        $("#btnSignIn").prop('disabled', true);
+    $("#btnSignIn").button('loading');
+    $("#btnSignIn").prop('disabled', true);
 
-        var obj = {
-            MobileNumber: $('#MobileNumber').val(),
-            Password: $('#Password').val(),
-            CountryCode: $("#MobileNumber").intlTelInput("getSelectedCountryData").dialCode
-        }
-        $.ajax({
-            url: SERVER + 'user/login',
-            type: 'post',
-            data: JSON.stringify(obj),
-            contentType: "application/json;charset=utf-8",
-            dataType: "json",
-            success: function (result) {
-                
-                $("#btnSignIn").prop('disabled', false);
-                $("#btnSignIn").button('reset');
-
-                if (!result.IsSuccess) {
-                    // TODO Muneeb: add proper bootstrap alert instead of browser alert
-                    //alert(result.Message);
-                    ShowAlert('Error', result.Message, 'danger');
-                    
-                    return;
-                }
-                if (result.ResponseData.UserType == "SUPERADMIN") {
-                    localStorage.setItem('UserType', "SUPERADMIN");
-                    window.location.replace('/index.html');
-                }
-                else if (result.ResponseData.UserType == "DOCTOR") {
-                    localStorage.setItem('UserType', "DOCTOR");
-                    localStorage.setItem('UserID', result.ResponseData.ID);
-                    localStorage.setItem('DoctorID', result.ResponseData.DoctorID);
-                    window.location.replace('/doctor/clinic-selection.html'); 
-                }
-                else if (result.ResponseData.UserType == "PARENT") {
-                    localStorage.setItem('UserType', "PARENT");
-                    localStorage.setItem('MobileNumber', result.ResponseData.MobileNumber);
-                    window.location.replace('/index.html');
-                }
-                else  {
-                    window.location.replace('/un-authorize.html');
-                }
-              
-            },
-            error: function (jqXHR, exception) {
-                $("#btnSignIn").prop('disabled', false);
-                $("#btnSignIn").button('reset');
-                displayErrors(jqXHR, exception);
-            }
-        });
+    var obj = {
+        MobileNumber: $('#MobileNumber').val(),
+        Password: $('#Password').val(),
+        CountryCode: $("#MobileNumber").intlTelInput("getSelectedCountryData").dialCode
     }
- 
+    console.log(obj);
+    $.ajax({
+        url: SERVER + 'user/login',
+        type: 'post',
+        data: JSON.stringify(obj),
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+
+            $("#btnSignIn").prop('disabled', false);
+            $("#btnSignIn").button('reset');
+
+            if (!result.IsSuccess) {
+                // TODO Muneeb: add proper bootstrap alert instead of browser alert
+                //alert(result.Message);
+                ShowAlert('Error', result.Message, 'danger');
+
+                return;
+            }
+            if (result.ResponseData.UserType == "SUPERADMIN") {
+                localStorage.setItem('UserType', "SUPERADMIN");
+                window.location.replace('/index.html');
+            }
+            else if (result.ResponseData.UserType == "DOCTOR") {
+                localStorage.setItem('UserType', "DOCTOR");
+                localStorage.setItem('UserID', result.ResponseData.ID);
+                localStorage.setItem('DoctorID', result.ResponseData.DoctorID);
+                window.location.replace('/doctor/clinic-selection.html');
+            }
+            else if (result.ResponseData.UserType == "PARENT") {
+                localStorage.setItem('UserType', "PARENT");
+                localStorage.setItem('MobileNumber', result.ResponseData.MobileNumber);
+                window.location.replace('/index.html');
+            }
+            else {
+                window.location.replace('/un-authorize.html');
+            }
+
+        },
+        error: function (jqXHR, exception) {
+            $("#btnSignIn").prop('disabled', false);
+            $("#btnSignIn").button('reset');
+            displayErrors(jqXHR, exception);
+        }
+    });
+}
