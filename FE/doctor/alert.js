93513a9fe4289feef33c89db991373e23a7e2d62
Muneeb Ur Rehman <munni7474@gmail.com>
2018-01-15 23:20:28 +0500

    child name and img in follow up alerts

--- FE/doctor/alert.js
+++ FE/doctor/alert.js
@@ -102,9 +102,69 @@ function loadData() {
                     }
                 } else {
                     markup = '<h3 style="color:red">No alert for today.</h3>';
+                    html = '<h4 style="color:red">No alert for today.</h4>';
                 }
                 $(".data").html(markup);
                 $("#childRecords").html(html);
+                HideAlert();
+            }
+        },
+        error: function (errormessage) {
+            ShowAlert('Error', errormessage.responseText, 'danger');
+        }
+    });
+
+    $.ajax({
+        url: SERVER + 'followup/alert/' + Id + '/' + OnlineClinic,
+        type: 'GET',
+        contentType: 'application/json;charset=utf-8',
+        dataType: "json",
+        success: function (result) {
+            if (!result.IsSuccess) {
+                ShowAlert('Error', result.Message, 'danger');
+            } else {
+                var html = '';
+                if (result.ResponseData.length != 0) {
+                    var map = {};
+                    $.each(result.ResponseData, function (key, item) {
+                        var obj = {
+                            Date: item.NextVisitDate,
+                            Gender: item.Child.Gender
+                        }
+                        if (item.Child.Name in map) {
+                            map[item.Child.Name].push(obj);
+                        } else {
+                            map[item.Child.Name] = [];
+                            map[item.Child.Name].push(obj);
+                        }
+
+
+                    });
+                    for (var key in map) {
+                        var arry = map[key];
+                        //
+                        html += '<div class="row">';
+                        html += '<div class="col-md-2">';
+                        html += '<img src="/img/'
+                        if (arry[0].Gender == "Boy")
+                            html += 'male.png"   class="img-responsive pull-right" alt="female" style="max-width:30px;max-height:30px" />';
+                        else
+                            html += 'female.png"   class="img-responsive pull-right" alt="female" style="max-width:30px;max-height:30px" />';
+
+                        html += ' </div>';
+                        html += ' <div class="col-md-8">';
+                        html += '<h5 style="border-bottom:solid 1px pink">' + key + '</h5>';
+                        html += '  <div class="pull-right">';
+                        html += '<i class="fa fa-phone" aria-hidden="true"></i>&nbsp;&nbsp;&nbsp;<i class="fa fa-envelope" aria-hidden="true"></i>';
+                        html += '</div>';
+                        html += '</div>';
+                        html += '</div>';
+
+                    }
+                } else {
+                    html = '<h4 style="color:red">No alert for today.</h4>';
+                }
+                $("#followupRecords").html(html);
                 HideAlert();
             }
         },
--- VaccineDose/Controllers/FollowUpController.cs
+++ VaccineDose/Controllers/FollowUpController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -33,5 +34,42 @@ namespace VaccineDose.Controllers
 
         }
         #endregion
+        [HttpGet]
+        [Route("api/followup/alert/{GapDays}/{OnlineClinicID}")]
+        public Response<IEnumerable<FollowUpDTO>> GetAlert(int GapDays, int OnlineClinicID)
+        {
+            try
+            {
+                using (VDConnectionString entities = new VDConnectionString())
+                {
+                    IEnumerable<FollowUp> followups = new List<FollowUp>();
+                    DateTime AddedDateTime = DateTime.Now.AddDays(GapDays);
+                    if (GapDays == 0)
+                        followups = entities.FollowUps.Include("Child")
+                            .Where(c => c.Child.ClinicID == OnlineClinicID)
+                            .Where(c => System.Data.Entity.DbFunctions.TruncateTime(c.NextVisitDate) == System.Data.Entity.DbFunctions.TruncateTime(DateTime.Now))
+                            .OrderBy(x => x.Child.ID).ThenBy(x => x.NextVisitDate).ToList<FollowUp>();
+                    else if (GapDays > 0)
+                        followups = entities.FollowUps.Include("Child")
+                            .Where(c => c.Child.ClinicID == OnlineClinicID)
+                            .Where(c => c.NextVisitDate >= DateTime.Now && c.NextVisitDate <= AddedDateTime)
+                            .OrderBy(x => x.Child.ID).ThenBy(x => x.NextVisitDate)
+                            .ToList<FollowUp>();
+                    else if (GapDays < 0)
+                        followups = entities.FollowUps.Include("Child")
+                            .Where(c => c.Child.ClinicID == OnlineClinicID)
+                            .Where(c => c.NextVisitDate <= DateTime.Now && c.NextVisitDate >= AddedDateTime)
+                            .OrderBy(x => x.Child.ID).ThenBy(x => x.NextVisitDate)
+                            .ToList<FollowUp>();
+                    IEnumerable<FollowUpDTO> followUpDTO = Mapper.Map<IEnumerable<FollowUpDTO>>(followups);
+                    return new Response<IEnumerable<FollowUpDTO>>(true, null, followUpDTO);
+                }
+            }
+            catch (Exception e)
+            {
+                return new Response<IEnumerable<FollowUpDTO>>(false, GetMessageFromExceptionObject(e), null);
+            }
+        }
+
     }
 }
