e51c5846b6b33c1b42b72df045d6c04022dad4a9
thezeeshanasghar <thezeeshanasghar@gmail.com>
2018-01-18 10:36:21 +0500

    update db changes sql script

--- DB/Scripts/DB-Changes.sql
+++ DB/Scripts/DB-Changes.sql
@@ -3,3 +3,9 @@ ADD GivenDate date
 
 alter table dose
 alter column MinAge int not null
+
+Alter table Doctor
+Add  [AllowInvoice] BIT NOT NULL DEFAULT(1), 
+	 [AllowChart] BIT NOT NULL DEFAULT(1), 
+     [AllowFollowUp] BIT NOT NULL DEFAULT(1), 
+     [AllowInventory] BIT NOT NULL DEFAULT (1)
