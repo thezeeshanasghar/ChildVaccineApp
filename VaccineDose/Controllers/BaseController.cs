5eb4d00a70c0670999fe7f83ef26270af614a931
thezeeshanasghar <thezeeshanasghar@gmail.com>
2018-04-23 09:29:14 +0500

    fix child listing page bugs

--- VaccineDose/Controllers/BaseController.cs
+++ VaccineDose/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,9 +13,16 @@ namespace VaccineDose.Controllers
         public static string GetMessageFromExceptionObject(Exception ex)
         {
             String message = ex.Message;
+            if(ex is DbEntityValidationException)
+            {
+                var errorMessages = ((DbEntityValidationException)ex).EntityValidationErrors.SelectMany(x => x.ValidationErrors).Select(x => x.ErrorMessage);
+                var fullErrorMessage = string.Join("; ", errorMessages);
+                return string.Concat(ex.Message, "<br />The validation errors are: ", fullErrorMessage);
+            }
             message += (ex.InnerException != null) ? ("<br />" + ex.InnerException.Message) : "";
             message += (ex.InnerException != null && ex.InnerException.InnerException != null) ? ("<br />" + ex.InnerException.InnerException.Message) : "";
             return message;
         }
     }
 }
+
