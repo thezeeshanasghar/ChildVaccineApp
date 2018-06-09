c0b989e7d28c38ac0fcccaac8b2f68509934d063
thezeeshanasghar <thezeeshanasghar@gmail.com>
2018-06-09 16:49:27 +0500

    .AddDays ==> caculateDate method

--- VaccineDose/Controllers/ChildController.cs
+++ VaccineDose/Controllers/ChildController.cs
@@ -184,35 +184,7 @@ namespace VaccineDose.Controllers
                 return new Response<ChildDTO>(false, GetMessageFromExceptionObject(e), null);
             }
         }
-        private DateTime calculateDate(DateTime date, int GapInDays)
-        {
-            // For 3 months
-            if (GapInDays == 84)
-                return date.AddMonths(3);
-            // For 9 Year 1 month
-            else if (GapInDays == 3315)
-                return date.AddYears(9).AddMonths(1);
-            // For 10 Year 6 month
-            else if (GapInDays == 3833)
-                return date.AddYears(10).AddMonths(6);
-            // For 1 to 15 years
-            else if (GapInDays == 365 || GapInDays == 730 || GapInDays == 1095 ||
-                GapInDays == 1460 || GapInDays == 1825 || GapInDays == 2190 || GapInDays == 2555 ||
-                GapInDays == 2920 || GapInDays == 3285 || GapInDays == 3650 || GapInDays == 4015 ||
-                GapInDays == 4380 || GapInDays == 4745 || GapInDays == 5110 || GapInDays == 5475)
-                return date.AddYears((int)(GapInDays / 365));
-            // From 6 months to 11 months
-            else if (GapInDays >= 168 || GapInDays <= 334)
-                return date.AddMonths((int)(GapInDays / 28));
-            // From 13 months to 20 months
-            else if (GapInDays >= 395 || GapInDays <= 608)
-                return date.AddMonths((int)(GapInDays / 29));
-            // From 21 months to 11 months
-            else if (GapInDays >= 639 || GapInDays <= 1795)
-                return date.AddMonths((int)(GapInDays / 30));
-            else
-                return date.AddDays(GapInDays);
-        }
+        
 
         public Response<ChildDTO> Put([FromBody] ChildDTO childDTO)
         {
