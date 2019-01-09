2ba814c60757acb20848e28019f4a4389ab83f25
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-01-09 08:57:30 +0500

    add new DashboardController and DTO

---
+++ VaccineDose/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VaccineDose.Model;
+
+namespace VaccineDose.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        public IHttpActionResult getCountOfDifferentCases()
+        {
+            try
+            {
+                using (VDEntities entities = new VDEntities())
+                {
+                    DashboardDTO dto = new DashboardDTO();
+
+                    dto.VaccineCount = entities.Vaccines.Count();
+                    dto.DoseCount = entities.Doses.Count();
+                    dto.BrandCount = entities.Brands.Count();
+                    dto.ChildCount = entities.Children.Count();
+                    dto.ApprovedDoctorCount = entities.Doctors.Where(e => e.IsApproved).Count();
+                    dto.UnApprovedDoctorCount = entities.Doctors.Count() - dto.ApprovedDoctorCount;
+
+                    return Ok(dto);
+                }
+            }
+            catch (Exception e)
+            {                 
+                return BadRequest(GetMessageFromExceptionObject(e));
+            }
+        }
+    }
+}
+
---
+++ VaccineDose/Model/DashboardDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace VaccineDose.Model
+{
+    public class DashboardDTO
+    {
+        public int VaccineCount { get; set; }
+        public int BrandCount { get; set; }
+        public int DoseCount { get; set; }
+        public int ChildCount { get; set; }
+        public int ApprovedDoctorCount { get; set; }
+        public int UnApprovedDoctorCount { get; set; }
+    }
+}
